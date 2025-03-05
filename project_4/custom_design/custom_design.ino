3055b331f740d79b2a36976f96a4b6987a1f4194
Josh Wild <jwild2099@gmail.com>
2025-03-04 19:04:46 -0600

    Update custom_design.ino

--- project_4/custom_design/custom_design.ino
+++ project_4/custom_design/custom_design.ino
@@ -1,7 +1,9 @@
 // Project 4: Custom Design
 
 // The purpose of this project was to create a custom design using a light sensor, FSR sensor, 
-// potentiometer, LED, motor, and buzzer.  
+// potentiometer, LED, motor, and buzzer. We decided to make a design where the user is meant to practice their grip strength with the FSR.
+// If the light sensor detects it is night time and the user is asleep, it vibrates the motor and flashes the light to wake them up. When the FSR sensor
+// detects the user is applying pressure, it tones the buzzer. The pitch of the buzzer may also be adjusted using the potentiometer.
 
 // Group Members: Josh Wild, Dayne Newman
 // Group Number: 14 (not sure this is what the bottom of the breadboard said)
