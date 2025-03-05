b563aadf18505493659fde6f1a44a1075c45408a
Josh Wild <jwild2099@gmail.com>
2025-03-04 19:02:41 -0600

    Update led_brightness_sensor.ino

--- project_4/led_brightness_sensor/led_brightness_sensor.ino
+++ project_4/led_brightness_sensor/led_brightness_sensor.ino
@@ -1,3 +1,15 @@
+// Project 4: Custom Design
+
+// The purpose of this project was to create a custom design using a light sensor, FSR sensor, 
+// potentiometer, LED, motor, and buzzer.  
+
+// Group Members: Josh Wild, Dayne Newman
+// Group Number: 14 (not sure this is what the bottom of the breadboard said)
+// Date: 3/4/2025
+// Code written by: Both team members 
+
+
+
 const int LIGHT_SENSOR_PIN = A0; // light sensor's pin
 const int LED_PIN = 11; // LED's pin
 int LedBrightness;
