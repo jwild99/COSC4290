8207879d8196b4cf297d34a8eacfc98878e13ad9
Josh Wild <jwild2099@gmail.com>
2025-03-05 15:43:30 -0600

    Update potentiometer.ino

--- project_4/potentiometer/potentiometer.ino
+++ project_4/potentiometer/potentiometer.ino
@@ -1,9 +1,6 @@
-// Project 4: Custom Design
+// Project 4: Potentiometer
 
-// The purpose of this project was to create a custom design using a light sensor, FSR sensor, 
-// potentiometer, LED, motor, and buzzer. We decided to make a design where the user is meant to practice their grip strength with the FSR.
-// If the light sensor detects it is night time and the user is asleep, it vibrates the motor and flashes the light to wake them up. When the FSR sensor
-// detects the user is applying pressure, it tones the buzzer. The pitch of the buzzer may also be adjusted using the potentiometer.
+// The purpose of this project was to create a design which changes the brightness of and LED based on the input reading from a potentiometer.
 
 // Group Members: Josh Wild, Dayne Newman
 // Group Number: 14 (not sure this is what the bottom of the breadboard said)
@@ -14,6 +11,7 @@
 const int ledPin = 9; // pin that the LED is attached to
 int analogValue = 0; // value read from the potentiometer
 int brightness = 0; // PWM pin that the LED is on.
+
 void setup() {
   // put your setup code here, to run once:
   // initialize serial communications at 9600 bps:
@@ -21,6 +19,7 @@ void setup() {
   // declare the led pin as an output:
   pinMode(ledPin, OUTPUT);
 }
+
 void loop() {
   // put your main code here, to run repeatedly:
   analogValue = analogRead(A0);// read the value
