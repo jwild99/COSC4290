58edfc551d6a4c6c3e8b1c40df3a4e8f91380ada
Josh Wild <jwild2099@gmail.com>
2025-03-05 15:41:33 -0600

    Update led_light_sensor.ino

--- project_4/led_light_sensor/led_light_sensor.ino
+++ project_4/led_light_sensor/led_light_sensor.ino
@@ -1,10 +1,6 @@
-// Project 4: Custom Design
+// Project 4: LED light sensor
 
-// The purpose of this project was to create a custom design using a light sensor, FSR sensor, 
-// potentiometer, LED, motor, and buzzer. We decided to make a design where the user is meant to practice their grip strength with the FSR.
-// If the light sensor detects it is night time and the user is asleep, it vibrates the motor and flashes the light to wake them up. When the FSR sensor
-// detects the user is applying pressure, it tones the buzzer. The pitch of the buzzer may also be adjusted using the potentiometer.
-
+// The purpose of this project was to create a design which turns on and off an LED based on the input reading from a light sensor.
 // Group Members: Josh Wild, Dayne Newman
 // Group Number: 14 (not sure this is what the bottom of the breadboard said)
 // Date: 3/4/2025
@@ -15,24 +11,31 @@
 const int LIGHT_SENSOR_PIN = A0; //light sensor's pin
 const int LED_PIN = 13; //LED's pin
 const int ANALOG_THRESHOLD = 500;
+
 // variables will change:
 int analogValue;
+
 void setup() {
   pinMode(LED_PIN, OUTPUT); // set arduino pin to output mode
   Serial.begin(9600);
 }
+
 void loop() {
   //read the input on analog pin
   analogValue = analogRead(LIGHT_SENSOR_PIN);
+  
   //compare analog value with the threshold
   if(analogValue < ANALOG_THRESHOLD){
     digitalWrite(LED_PIN, HIGH); // turn on LED
+    // print values
     Serial.println("Light on");
     Serial.print("Sensor Value: ");
     Serial.println(analogValue);
   }
   else{
     digitalWrite(LED_PIN, LOW); // turn off LED
+
+    // print values
     Serial.println("Light off");
     Serial.print("Sensor Value: ");
     Serial.println(analogValue);
