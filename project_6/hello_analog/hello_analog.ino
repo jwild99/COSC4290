19ba60566d968da99c2e7a9152922d103353ef94
Dayne Newman <159488022+daynenewman@users.noreply.github.com>
2025-04-01 13:03:36 -0500

    Update hello_analog.ino

--- project_6/hello_analog/hello_analog.ino
+++ project_6/hello_analog/hello_analog.ino
@@ -1,12 +1,16 @@
-///////////////////////////////////////////////////////////////////////////////
-// Circuit Playground Analog In - Serial Print
-//
-// Author: Carter Nelson
-// MIT License (https://opensource.org/licenses/MIT)
+// Project 6: Part 4.a.II "Hello Analog"
+
+// The purpose of this project was to read the analog value from a potentiometer 
+// and display it on the serial monitor.
+
+// Group Members: Josh Wild, Dayne Newman  
+// Group Number: 14 (not sure this is what the bottom of the breadboard said)  
+// Date: 3/31/2025  
+// Code written by: Both team members  
 
 #include <Adafruit_CircuitPlayground.h>
 
-uint16_t value;
+uint16_t value; //store potentiometer reading
 
 ///////////////////////////////////////////////////////////////////////////////
 void setup() {
@@ -16,7 +20,7 @@ void setup() {
 
 ///////////////////////////////////////////////////////////////////////////////
 void loop() {
-  value = analogRead(3);
-  Serial.println(value);
-  delay(500);
+  value = analogRead(3); //reading input from potentiometer
+  Serial.println(value); //print value
+  delay(500); //wait 500ms
 }
