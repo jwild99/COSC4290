0e2e0c69616a632c8b5a2664a3154a39916c4a83
Dayne Newman <159488022+daynenewman@users.noreply.github.com>
2025-04-01 15:42:43 -0500

    Update Hello_Speaker.ino

--- project_6/Hello_Speaker/Hello_Speaker.ino
+++ project_6/Hello_Speaker/Hello_Speaker.ino
@@ -1,3 +1,12 @@
+// Project 6: Part 4.a.I "Hello Analog"
+//
+// The purpose of this project was to show the basic sound output using the Circuit Playground's built-in speaker. 
+//
+// Group Members: Josh Wild, Dayne Newman  
+// Group Number: 14 (not sure this is what the bottom of the breadboard said)  
+// Date: 3/31/2025  
+// Code written by: Both team members 
+
 #include <Adafruit_CircuitPlayground.h>
 
 void setup() {
@@ -5,6 +14,6 @@ void setup() {
 }
 
 void loop() {
-  CircuitPlayground.playTone(500, 100);
-  delay(1000);
+  CircuitPlayground.playTone(500, 100); //play tone
+  delay(1000); //wait 1 second
 }
