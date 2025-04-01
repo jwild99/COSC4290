c23a37887cd8cad90fd38b966762e96818444da6
Dayne Newman <159488022+daynenewman@users.noreply.github.com>
2025-04-01 12:57:13 -0500

    Update neopixel_fun.ino

--- project_6/neopixel_fun/neopixel_fun.ino
+++ project_6/neopixel_fun/neopixel_fun.ino
@@ -1,13 +1,17 @@
-///////////////////////////////////////////////////////////////////////////////
-// Circuit Playground Analog In - NeoPixel Fun
-//
-// Author: Carter Nelson
-// MIT License (https://opensource.org/licenses/MIT)
+// Project 6: Part 4.a.III "Neopixel Fun"
+
+// The purpose of this project was to use a potentiometer to control how many NeoPixels 
+// light up on the Circuit Playground.
+
+// Group Members: Josh Wild, Dayne Newman  
+// Group Number: 14 (not sure this is what the bottom of the breadboard said)  
+// Date: 3/31/2025  
+// Code written by: Both team members  
 
 #include <Adafruit_CircuitPlayground.h>
 
-uint16_t value;
-uint8_t pixels;
+uint16_t value; //input value from potentiometer
+uint8_t pixels; //determines how many pixels light up
 
 ///////////////////////////////////////////////////////////////////////////////
 void setup() {
@@ -17,12 +21,12 @@ void setup() {
 
 ///////////////////////////////////////////////////////////////////////////////
 void loop() {
-  value = analogRead(3);
-  pixels = map(value, 0, 1023, 0, 10);
+  value = analogRead(3); //reading input from potentiometer
+  pixels = map(value, 0, 1023, 0, 10); //mapping value to range for number of NeoPixels
 
-  CircuitPlayground.clearPixels();
-  for (int p=0; p<pixels; p++) {
+  CircuitPlayground.clearPixels(); //turning off all pixels
+  for (int p=0; p<pixels; p++) { //looping through and lighting up the number of pixels
     CircuitPlayground.setPixelColor(p, 0xFF00FF);
   }
-  delay(100);
+  delay(100); //wait 100ms
 }
