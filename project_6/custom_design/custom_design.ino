b6e107ecd289086995f37a4635ff3db2ff079366
Dayne Newman <159488022+daynenewman@users.noreply.github.com>
2025-04-01 15:28:16 -0500

    Update custom_design.ino

--- project_6/custom_design/custom_design.ino
+++ project_6/custom_design/custom_design.ino
@@ -1,3 +1,18 @@
+// Project 6: Part 4.d.
+
+// The purpose of this project was to use the Circuit Playground's accelerometer 
+// to detect tilt direction and respond with a specific NeoPixel color:
+//  - Left Tilt: Blue
+//  - Right Tilt: Red
+//  - Forward Tilt: Green
+//  - Backward Tilt: Yellow
+//  - Flat: White
+//
+// Group Members: Josh Wild, Dayne Newman  
+// Group Number: 14 (not sure this is what the bottom of the breadboard said)  
+// Date: 3/31/2025  
+// Code written by: Both team members 
+
 #include <Adafruit_CircuitPlayground.h>
 
 void setup() {
@@ -5,9 +20,10 @@ void setup() {
 }
 
 void loop() {
-  float x = CircuitPlayground.motionX();
-  float y = CircuitPlayground.motionY();
+  float x = CircuitPlayground.motionX(); //read x axis acceleration
+  float y = CircuitPlayground.motionY(); //read y axis acceleration
 
+  //checking tilt direction and changing color accordingly 
   if (x < -3) {
     setColor(0, 0, 255);  // Blue (Tilt Left)
   } else if (x > 3) {
@@ -20,9 +36,10 @@ void loop() {
     setColor(255, 255, 255);  // White (Flat)
   }
 
-  delay(100);
+  delay(100); //wait 100ms
 }
 
+//Setting all NeoPixels to a specific color
 void setColor(int r, int g, int b) {
   for (int i = 0; i < 10; i++) {
     CircuitPlayground.setPixelColor(i, r, g, b);
