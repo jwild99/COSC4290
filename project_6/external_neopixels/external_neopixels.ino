7df0a8da0596fb11ba838ecba2339155a933de2f
Dayne Newman <159488022+daynenewman@users.noreply.github.com>
2025-04-01 13:13:24 -0500

    Update external_neopixels.ino

--- project_6/external_neopixels/external_neopixels.ino
+++ project_6/external_neopixels/external_neopixels.ino
@@ -1,7 +1,19 @@
+// Project 6: Part 3 "External NeoPixels"
+
+// The purpose of this project was to control both the onboard NeoPixels and an external NeoPixel strip 
+// using the Circuit Playground Express.
+
+// Group Members: Josh Wild, Dayne Newman  
+// Group Number: 14 (not sure this is what the bottom of the breadboard said)  
+// Date: 3/31/2025  
+// Code written by: Both team members  
+
+
 #include <Adafruit_CircuitPlayground.h>
 // do NOT include the standard NeoPixel library
-
-#define NEOPIX_PIN    A2
+//controls NeoPixel strip
+#define NEOPIX_PIN    A2 
+//Number of pixels in strip
 #define NUM_PIXELS    5
 
 // use Adafruit_CPlay_NeoPixel to create a separate external NeoPixel strip
@@ -21,32 +33,32 @@ void loop() {
   CircuitPlayground.clearPixels();
   
   // for the external NeoPixels, must use the Adafruit_CPlay_NeoPixel functions directly
-  strip.clear();
-  strip.show();
+  strip.clear(); //clear external NeoPixels
+  strip.show(); //clears external strip
 
-  delay(500);
+  delay(500); //wait 500ms
 
-  CircuitPlayground.setPixelColor(0, 255,   0,   0);
-  CircuitPlayground.setPixelColor(1, 128, 128,   0);
-  CircuitPlayground.setPixelColor(2,   0, 255,   0);
-  CircuitPlayground.setPixelColor(3,   0, 128, 128);
-  CircuitPlayground.setPixelColor(4,   0,   0, 255);
+  CircuitPlayground.setPixelColor(0, 255,   0,   0); //Red
+  CircuitPlayground.setPixelColor(1, 128, 128,   0); //Olive
+  CircuitPlayground.setPixelColor(2,   0, 255,   0); //Green
+  CircuitPlayground.setPixelColor(3,   0, 128, 128); //Teal
+  CircuitPlayground.setPixelColor(4,   0,   0, 255); //Blue
   
-  CircuitPlayground.setPixelColor(5, 0xFF0000);
-  CircuitPlayground.setPixelColor(6, 0x808000);
-  CircuitPlayground.setPixelColor(7, 0x00FF00);
-  CircuitPlayground.setPixelColor(8, 0x008080);
-  CircuitPlayground.setPixelColor(9, 0x0000FF);
+  CircuitPlayground.setPixelColor(5, 0xFF0000); //Red
+  CircuitPlayground.setPixelColor(6, 0x808000); //Olive
+  CircuitPlayground.setPixelColor(7, 0x00FF00); //Green
+  CircuitPlayground.setPixelColor(8, 0x008080); //Teal
+  CircuitPlayground.setPixelColor(9, 0x0000FF); //Blue
 
   // some functions are the same
-  strip.setPixelColor(0, 255,   0,   0);
-  strip.setPixelColor(1, 128, 128,   0);
-  strip.setPixelColor(2,   0, 255,   0);
-  strip.setPixelColor(3,   0, 128, 128);
-  strip.setPixelColor(4,   0,   0, 255);
+  strip.setPixelColor(0, 255,   0,   0); //Red
+  strip.setPixelColor(1, 128, 128,   0); //Olive
+  strip.setPixelColor(2,   0, 255,   0); //Green
+  strip.setPixelColor(3,   0, 128, 128); //Teal
+  strip.setPixelColor(4,   0,   0, 255); //Blue
   
   // but for the external strip, must call show()
   strip.show();
 
-  delay(5000);
+  delay(5000); //wait 5 seconds
 }
